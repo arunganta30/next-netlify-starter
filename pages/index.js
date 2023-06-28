850094b84a1ea92824824c3a9f8c26f191781f77
arunganta30 <138011404+arunganta30@users.noreply.github.com>
2023-06-28 17:16:00 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,11 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Skyward canyons" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
-
-    Skyward canyons rise majestically above the rugged landscape, defying gravity and captivating the imagination. These towering geological formations, often found in remote and untouched corners of the world, beckon adventurers and nature enthusiasts alike. Carved by the forces of wind and water over millions of years, these canyons are characterized by their sheer vertical walls that seem to reach for the sky. Standing at the edge of a skyward canyon, one can't help but feel a sense of awe and wonder at the grandeur of nature's sculpting prowess.
+<a href="https://www.techspurblog.com/skyward-canyons-login/">Skyward canyons</a> rise majestically above the rugged landscape, defying gravity and captivating the imagination. These towering geological formations, often found in remote and untouched corners of the world, beckon adventurers and nature enthusiasts alike. Carved by the forces of wind and water over millions of years, these canyons are characterized by their sheer vertical walls that seem to reach for the sky. Standing at the edge of a skyward canyon, one can't help but feel a sense of awe and wonder at the grandeur of nature's sculpting prowess.
         </p>
       </main>
 
